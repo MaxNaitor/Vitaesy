ff1c873f7ca4fde17b56ef2e1bf9c12eb6d1e8bc
Tiziano Massa <tiziano.max@gmail.com>
2021-05-11 23:17:29 +0200

    stampa funzionante

--- src/app/template/template.component.ts
+++ src/app/template/template.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
 
 @Component({
   selector: 'app-template',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateComponent implements OnInit {
 
+  @ViewChild('cv') cv: ElementRef;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  generatePdf(nome:string,cognome:string) {
+    let data = document.getElementById('cv');
+    html2canvas(data, { allowTaint: false }).then(canvas => {
+      let HTML_Width = canvas.width;
+      let HTML_Height = canvas.height;
+      let top_left_margin = 15;
+      let PDF_Width = HTML_Width + (top_left_margin * 2);
+      let PDF_Height = (PDF_Width * 1.5) + (top_left_margin * 2);
+      let canvas_image_width = HTML_Width;
+      let canvas_image_height = HTML_Height;
+      let totalPDFPages = Math.ceil(HTML_Height / PDF_Height) - 1;
+      canvas.getContext('2d');
+      let imgData = canvas.toDataURL("image/jpeg", 1.0);
+      let pdf = new jsPDF('p', 'pt', [PDF_Width, PDF_Height]);
+      pdf.addImage(imgData, 'JPG', top_left_margin, top_left_margin, canvas_image_width, canvas_image_height);
+      for (let i = 1; i <= totalPDFPages; i++) {
+        pdf.addPage([PDF_Width, PDF_Height], 'p');
+        pdf.addImage(imgData, 'JPG', top_left_margin, -(PDF_Height * i) + (top_left_margin * 4), canvas_image_width, canvas_image_height);
+      }
+      pdf.save('CV ' + nome + ' ' + cognome + '.pdf');
+    });
   }
 
 }
